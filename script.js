e6cf43b502f6165b87aa18d25c82afd4770e821d
Gopi2301 <118594301+Gopi2301@users.noreply.github.com>
2022-12-22 01:10:47 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-var display = document.querySelector(".display");
+var display = document.querySelector(".data");
 
+// Mouse Click
 function calcfun(e){
-    var value_calc= display.innerText
-    console.log(e.innerText);
-    display.innerText = value_calc+e.innerText
+    var value_calc= display.innerText;
+    var last_val = value_calc.slice(-1)[0];
+    var expression = ['+','-','*','/']
+    if (!expression.includes(last_val)){
+        display.innerText = value_calc+e.innerText;
+    } else if (!expression.includes(e.innerText)){
+        display.innerText = value_calc+e.innerText;
+        
+    } 
 }
+
+
+document.querySelector('body').addEventListener('keypress', (event)=>{
+    if (event.code.includes("Digit")|| event.code.includes("Numpad")){
+    var value_calc= display.innerText;
+        
+        display.innerText=  value_calc+event.key;
+    }
+})
+
+
