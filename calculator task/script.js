2b8f23c06215bc2d1c72371c3cf5fba95da0fbc7
Gopi2301 <118594301+Gopi2301@users.noreply.github.com>
2022-12-22 18:50:09 +0530

    functionality updated
    js functionality updated

---
+++ calculator task/script.js
@@ -0,0 +1,31 @@
+var display = document.querySelector(".data");
+
+// Mouse Click
+function calcfun(e){
+    var value_calc= display.innerText;
+    var last_val = value_calc.slice(-1)[0];
+    var expression = ['+','-','*','/']
+    if (!expression.includes(last_val)){
+        display.innerText = value_calc+e.innerText;
+    } else if (!expression.includes(e.innerText)){
+        display.innerText = value_calc+e.innerText;
+        
+    } 
+}
+
+
+document.querySelector('body').addEventListener('keypress', (event)=>{
+    if (event.code.includes("Digit")|| event.code.includes("Numpad")){
+    var value_calc= display.innerText;
+        
+        display.innerText=  value_calc+event.key;
+    }
+})
+function resfun (e){
+    var result =eval(display.innerText)
+    display.innerText= result
+}
+
+function clearfun(e){
+    display.innerText=""
+}
